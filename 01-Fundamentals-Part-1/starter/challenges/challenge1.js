17459861312ece271f30116efabd636389892369
SergiiPachkovkyi <SergiiPachkovkyi@gmail.com>
2023-02-03 13:44:50 +0200

    challenge 1

---
+++ 01-Fundamentals-Part-1/starter/challenges/challenge1.js
@@ -0,0 +1,20 @@
+// data 1
+let weightMark = 78;
+let heightMark = 1.69;
+
+let weightJohn = 92;
+let heightJohn = 1.95;
+
+// //data 2
+// weightMark = 95;
+// heightMark = 1.88;
+
+// weightJohn = 85;
+// heightJohn = 1.76;
+
+let markBMI = weightMark / heightMark ** 2
+let johnBMI = weightJohn / heightJohn ** 2
+
+markHigherBMI = markBMI > johnBMI
+
+console.log(markBMI, johnBMI, markHigherBMI);
